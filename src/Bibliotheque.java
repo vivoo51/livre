ebe62e4902ec720150547c6a307e850b0e638a1d
vivoo51 <vivoo51@gmail.com>
2013-03-11 13:23:54 +0100

    commit

---
+++ src/Bibliotheque.java
@@ -0,0 +1,60 @@
+/**
+ * Classe representant le stock de livre
+ * 
+ * @author Vivien Laurent
+ * 
+ */
+public class Bibliotheque {
+	/**
+	 * Attribut de type Liste, representant la liste des livres dans la
+	 * bibliotheque
+	 */
+	private Liste listeLivre;
+
+	/**
+	 * Constructeur initialisant une bibliotheque
+	 */
+	public Bibliotheque() {
+		this.listeLivre = new Liste();
+
+	}
+
+	/**
+	 * Methode permettant l'ajout d'un Roman dans la Bibliotheque
+	 * 
+	 * @param titre
+	 * @param auteur
+	 */
+	public void ajouteRoman(String auteur, String titre) {
+		listeLivre.ajouter(new Roman(titre, auteur));
+	}
+
+	/**
+	 * Methode permettant l'ajout d'un Comics dans la Bibliotheque
+	 * 
+	 * @param auteur
+	 *            Auteur du Comics
+	 * @param dessinateur
+	 *            Dessinateur du Comics
+	 * @param titre
+	 *            Titre du Comics
+	 */
+	public void ajouteComics(String auteur, String dessinateur, String titre) {
+		listeLivre.ajouter(new Comics(titre, auteur, dessinateur));
+	}
+
+	/**
+	 * Methode permettant l'ajout d'un Mangadans la Bibliotheque
+	 * 
+	 * @param auteur
+	 *            Auteur du manga
+	 * @param dessinateur
+	 *            Dessinateur du Manga
+	 * @param titre
+	 *            Titre du Manga
+	 */
+	public void ajouteManga(String auteur, String dessinateur, String titre) {
+		listeLivre.ajouter(new Manga(titre, auteur, dessinateur));
+	}
+
+}
